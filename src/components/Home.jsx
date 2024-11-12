8b0e01f0d56fa602d3dd7f39e0587f0b0f5f1108
sharunraj <sharunraj39@gmail.com>
2024-11-12 09:05:24 +0530

    theme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="bg-gray-50 min-h-screen">
-      {/* Hero Section */}
-      <header className="bg-gradient-to-r from-indigo-600 to-blue-500 text-white py-20">
+      {/* Hero Section bg-gradient-to-r from-indigo-600 to-blue-500 */}
+      <header className="bg-gradient-to-r from-Tertiary to-Primary text-Secondary py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <h1 className="text-4xl md:text-5xl font-bold">
             Welcome to HealthTrackerApp
@@ -19,13 +19,13 @@ const Home = () => {
           </p>
           <div className="mt-8 space-x-4">
             <button
-              className="bg-white text-indigo-600 font-semibold px-6 py-2 rounded-md shadow-md hover:bg-gray-200 transition"
+              className="bg-white text-Primary font-semibold px-6 py-2 rounded-md shadow-md hover:text-Quaternary hover:bg-gray-200 transition"
               onClick={() => navigate(`/signup`)}
             >
               Sign Up
             </button>
             <button
-              className="bg-transparent border-2 border-white text-white font-semibold px-6 py-2 rounded-md hover:bg-white hover:text-indigo-600 transition"
+              className="bg-transparent border-2 border-white text-white font-semibold px-6 py-2 rounded-md hover:bg-Quaternary hover:text-Primary transition"
               onClick={() => navigate(`/login`)}
             >
               Login
@@ -34,14 +34,14 @@ const Home = () => {
         </div>
       </header>
       {/* Features Section */}
-      <section className="py-16 bg-gray-50">
+      <section className="py-16 bg-Secondary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-gray-900 text-center mb-8">
             What We Offer
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {/* Card 1 - Fitness Tracking */}
-            <div className="bg-white rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
+            <div className="bg-Primary rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
               <div className="relative h-56">
                 <img
                   className="w-full h-full object-cover group-hover:scale-105 transition-all"
@@ -50,11 +50,11 @@ const Home = () => {
                 />
               </div>
               <div className="p-6">
-                <h3 className="text-xl font-semibold text-indigo-600 flex items-center space-x-2">
+                <h3 className="text-xl font-semibold text-Quaternary flex items-center space-x-2">
                   <IconGym />
                   <span>Fitness Tracking</span>
                 </h3>
-                <p className="mt-2 text-gray-600">
+                <p className="mt-2 text-Secondary">
                   Log your workouts, set fitness goals, and track your progress
                   with personalized insights.
                 </p>
@@ -62,7 +62,7 @@ const Home = () => {
             </div>
 
             {/* Card 2 - Diet Management */}
-            <div className="bg-white rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
+            <div className="bg-Primary rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
               <div className="relative h-56">
                 <img
                   className="w-full h-full object-cover group-hover:scale-105 transition-all"
@@ -71,11 +71,11 @@ const Home = () => {
                 />
               </div>
               <div className="p-6">
-                <h3 className="text-xl font-semibold text-indigo-600 flex items-center space-x-2">
+                <h3 className="text-xl font-semibold text-Quaternary flex items-center space-x-2">
                   <IconFoodOutline />
                   <span>Diet Management</span>
                 </h3>
-                <p className="mt-2 text-gray-600">
+                <p className="mt-2 text-Secondary">
                   Maintain a balanced diet with calorie tracking and meal
                   recommendations tailored to your goals.
                 </p>
@@ -83,7 +83,7 @@ const Home = () => {
             </div>
 
             {/* Card 3 - Well-being Insights */}
-            <div className="bg-white rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
+            <div className="bg-Primary rounded-lg shadow-lg overflow-hidden group hover:shadow-2xl transition">
               <div className="relative h-56">
                 <img
                   className="w-full h-full object-cover group-hover:scale-105 transition-all"
@@ -92,11 +92,11 @@ const Home = () => {
                 />
               </div>
               <div className="p-6">
-                <h3 className="text-xl font-semibold text-indigo-600 flex items-center space-x-2">
+                <h3 className="text-xl font-semibold text-Quaternary flex items-center space-x-2">
                   <IconMentalHealthFill />
                   <span>Well-being Insights</span>
                 </h3>
-                <p className="mt-2 text-gray-600">
+                <p className="mt-2 text-Secondary">
                   Monitor your mental health with mood tracking and self-care
                   reminders to ensure holistic wellness.
                 </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <>
-      <nav className="bg-white">
+      <nav className="bg-Primary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16 items-center">
             {/* Left side - Logo and App Name */}
@@ -14,37 +14,31 @@ const Navbar = () => {
               <IconHeartbeat
                 style={{ width: "24px", height: "24px", color: "#ff0000" }}
               />
-              <span className="text-black ml-2 text-lg font-semibold">
+              <span className="text-Secondary ml-2 text-lg font-semibold">
                 HealthTrackerApp
               </span>
             </div>
 
             {/* Right side - Navigation Links */}
             <div className="hidden md:flex items-center space-x-4">
-              <a
-                
-                className="text-black hover:bg-blue-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
+              <a className="text-Secondary hover:bg-Quaternary hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Dashboard
               </a>
               <a
-                
-                className="text-black hover:bg-blue-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-Secondary hover:bg-Quaternary hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 onClick={() => navigate(`/fitness`)}
               >
                 Fitness
               </a>
               <a
-                
-                className="text-black hover:bg-blue-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-Secondary hover:bg-Quaternary hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 onClick={() => navigate(`/diet`)}
               >
                 Diet
               </a>
               <a
-                
-                className="text-black hover:bg-blue-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                onClick={()=>navigate(`/wellbeing`)}
+                className="text-Secondary hover:bg-Quaternary hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                onClick={() => navigate(`/wellbeing`)}
               >
                 Wellbeing
               </a>
@@ -52,7 +46,7 @@ const Navbar = () => {
               {/* Profile Photo */}
               <div className="relative">
                 <img
-                  className="h-8 w-8 rounded-full border-2 border-white hover:border-blue-500 cursor-pointer"
+                  className="h-8 w-8 rounded-full border-2 border-white hover:border-Quaternary cursor-pointer"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt="Profile"
                 />
