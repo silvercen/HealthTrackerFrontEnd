bf3786f460db54167347d46c89cf9e32df352658
sharunraj <sharunraj39@gmail.com>
2024-11-21 23:46:50 +0530

    dashboard styling

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -348,9 +348,6 @@ const Dashboard = () => {
 
   return (
     <div className="min-h-screen font-poppins p-8">
-      <h1 className="text-4xl underline font-bold  text-center text-Quaternary mb-8">
-        Dashboard
-      </h1>
       {/* User Info Section */}
       <div className="bg-Grey bg-opacity-40 backdrop-blur-lg text-Secondary p-6 rounded-lg shadow-md mb-8 flex flex-col md:flex-row items-center">
         {/* User Details */}
@@ -379,7 +376,7 @@ const Dashboard = () => {
           </p>
           <p className="mb-2">
             <strong className="text-2xl font-medium pr-2">Journey:</strong>{" "}
-            <span className="text-2xl">{userInfo.journey}</span>
+            <span className="text-2xl text-Quaternary">{userInfo.journey}</span>
           </p>
           <p className="mt-4">
             <strong className="text-2xl font-medium pr-2">
@@ -388,23 +385,174 @@ const Dashboard = () => {
             <span className="text-2xl">{caloriesRequired.toFixed(1)} kcal</span>
           </p>
         </div>
-
-        {/* User Image */}
-        <div className="flex-1 flex justify-center mt-6 md:mt-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="rounded-full text-Secondary bg-opacity-40 shadow-lg w-48 h-48 object-cover border-4 border-Quaternary"
-          >
-            <path
-              fillRule="evenodd"
-              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
+        {/* Calorie Balance Section */}
+        <div className="bg-White hover:scale-105 transition bg-Grey bg-opacity-40 backdrop-blur-lg border-2 border-Quaternary p-4 rounded-lg shadow-md mt-4">
+          <h3 className="text-4xl font-semibold text-Quaternary mb-2">
+            Calorie Balance
+          </h3>
+          {dietData && fitnessData ? (
+            <div className="text-Secondary">
+              <p>
+                <strong>Total Calories Consumed:</strong>{" "}
+                <span className="text-Quaternary">
+                  {dietData.totalCaloriesConsumed.toFixed(2)}
+                </span>{" "}
+                kcal
+              </p>
+              <p>
+                <strong>Total Calories Burned:</strong>{" "}
+                <span className="text-Quaternary">
+                  {fitnessData.totalCaloriesBurned.toFixed(2)}
+                </span>{" "}
+                kcal
+              </p>
+              <p>
+                <strong>Net Calories:</strong>{" "}
+                <span className="text-Quaternary">
+                  {dietData.totalCaloriesConsumed -
+                    fitnessData.totalCaloriesBurned.toFixed(2)}
+                </span>{" "}
+                kcal
+              </p>
+              <p>
+                <strong>Maintenance Calories:</strong>{" "}
+                <span className="text-Quaternary">
+                  {caloriesRequired.toFixed(1)}
+                </span>{" "}
+                kcal
+              </p>
+              <p className="mt-4 ">
+                <strong className="text-xl">Result:</strong>{" "}
+                {(() => {
+                  const netCalories =
+                    dietData.totalCaloriesConsumed -
+                    fitnessData.totalCaloriesBurned;
+                  if (netCalories > caloriesRequired) {
+                    return (
+                      <span className="text-Quaternary text-xl font-bold">
+                        You are in a calorie surplus.{" "}
+                        {userInfo.journey === "WEIGHT_LOSS"
+                          ? "This may hinder weight loss."
+                          : userInfo.journey === "WEIGHT_GAIN"
+                          ? "Great for gaining weight!"
+                          : "You may gain weight."}
+                      </span>
+                    );
+                  } else if (netCalories < caloriesRequired) {
+                    return (
+                      <span className="text-Quaternary text-xl font-bold">
+                        You are in a calorie deficit.{" "}
+                        {userInfo.journey === "WEIGHT_LOSS"
+                          ? "This is ideal for weight loss!"
+                          : userInfo.journey === "WEIGHT_GAIN"
+                          ? "This may hinder weight gain."
+                          : "You may lose weight."}
+                      </span>
+                    );
+                  } else {
+                    return (
+                      <span className="text-Quaternary text-xl font-bold ">
+                        You are meeting your maintenance calories.{" "}
+                        {userInfo.journey === "WEIGHT_LOSS"
+                          ? "Consider reducing your calories for weight loss."
+                          : userInfo.journey === "WEIGHT_GAIN"
+                          ? "Consider increasing your calories for weight gain."
+                          : "You are maintaining your weight."}
+                      </span>
+                    );
+                  }
+                })()}
+              </p>
+            </div>
+          ) : (
+            <p className="text-Secondary">Calorie data is not available.</p>
+          )}
         </div>
       </div>
+      {reportSummary && reportSummary.length > 0 ? (
+        <>
+          <h1 className="text-3xl underline font-semibold mt-6 text-center text-Quaternary mb-8">
+            Weekly Report
+          </h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* Fitness Chart */}
+            <div className="bg-White hover:scale-105 transition bg-Grey bg-opacity-40 backdrop-blur-lg p-4 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold text-Quaternary mb-4">
+                Weekly Fitness Report
+              </h2>
+              <Bar data={fitnessChartData} options={{ responsive: true }} />
+            </div>
+
+            {/* Diet Chart */}
+            <div className="bg-White bg-Grey hover:scale-105 transition bg-opacity-40 backdrop-blur-lg p-4 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold text-Quaternary mb-4">
+                Weekly Diet Report
+              </h2>
+              <Line data={dietChartData} options={{ responsive: true }} />
+            </div>
+
+            {/* Sleep Chart */}
+            <div className="bg-White bg-Grey hover:scale-105 transition bg-opacity-40 backdrop-blur-lg p-4 rounded-lg shadow-md">
+              <h2 className="text-2xl font-bold text-Quaternary mb-4">
+                Weekly Sleep Report
+              </h2>
+              <Bar data={wellbeingChartData} options={{ responsive: true }} />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="text-center text-xl mb-6 text-Grey mt-12">
+          No weekly report data available.
+        </div>
+      )}
+
+      {/* Report Summary Section */}
+      {reportSummary && reportSummary.length > 0 && (
+        <div className="bg-White bg-Grey bg-opacity-40 mt-7 backdrop-blur-lg border-2 border-Quaternary p-6 rounded-lg shadow-md mb-8">
+          <h3 className="text-4xl font-semibold text-Quaternary mb-4">
+            Weekly Report Summary
+          </h3>
+          <ul className="text-Secondary space-y-4">
+            {reportSummary.map((summary, index) => (
+              <li key={index} className="p-4">
+                <p>
+                  <strong>Calories Consumed This Week:</strong>{" "}
+                  <span className="text-Quaternary">
+                    {summary.CaloriesConsumedWeek.toFixed(2)}
+                  </span>
+                </p>
+                <p>
+                  <strong>Calories Burned This Week:</strong>{" "}
+                  <span className="text-Quaternary">
+                    {summary.CaloriesBurnedWeek.toFixed(2)}
+                  </span>
+                </p>
+
+                <p>
+                  <strong>Your Calories:</strong>{" "}
+                  <span className="text-Quaternary">
+                    {summary.UserCalories.toFixed(2)}
+                  </span>{" "}
+                  kcal
+                </p>
+                <p className="text-xl">
+                  <strong>Overall Status:</strong>{" "}
+                  <span
+                    className={`font-bold ${
+                      summary.Status === "Healthy"
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {summary.Status}
+                  </span>
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       {/* Today's Activity Section */}
       <div className="grid grid-cols-1 font-poppins md:grid-cols-3 gap-4">
@@ -422,15 +570,15 @@ const Dashboard = () => {
                     {workout.workoutType})
                   </p>
                   <p>
-                    <strong>Calories Burned:</strong> {workout.caloriesBurned}{" "}
-                    kcal
+                    <strong>Calories Burned:</strong>{" "}
+                    {workout.caloriesBurned.toFixed(2)} kcal
                   </p>
                 </li>
               ))}
               <li className="font-bold text-2xl">
                 Total Calories Burned:{" "}
                 <span className="text-Quaternary">
-                  {fitnessData.totalCaloriesBurned}
+                  {fitnessData.totalCaloriesBurned.toFixed(2)}
                 </span>{" "}
                 kcal
               </li>
@@ -493,170 +641,6 @@ const Dashboard = () => {
             {wellbeingData?.sleepTime || "Not logged"} hrs
           </p>
         </div>
-      </div>
-      {/* Calorie Balance Section */}
-      <div className="bg-White bg-Grey bg-opacity-40 backdrop-blur-lg border-2 border-Quaternary p-4 rounded-lg shadow-md mt-4">
-        <h3 className="text-4xl font-semibold text-Quaternary mb-2">
-          Calorie Balance
-        </h3>
-        {dietData && fitnessData ? (
-          <div className="text-Secondary">
-            <p>
-              <strong>Total Calories Consumed:</strong>{" "}
-              <span className="text-Quaternary">
-                {dietData.totalCaloriesConsumed}
-              </span>{" "}
-              kcal
-            </p>
-            <p>
-              <strong>Total Calories Burned:</strong>{" "}
-              <span className="text-Quaternary">
-                {fitnessData.totalCaloriesBurned}
-              </span>{" "}
-              kcal
-            </p>
-            <p>
-              <strong>Net Calories:</strong>{" "}
-              <span className="text-Quaternary">
-                {dietData.totalCaloriesConsumed -
-                  fitnessData.totalCaloriesBurned}
-              </span>{" "}
-              kcal
-            </p>
-            <p>
-              <strong>Maintenance Calories:</strong>{" "}
-              <span className="text-Quaternary">
-                {caloriesRequired.toFixed(1)}
-              </span>{" "}
-              kcal
-            </p>
-            <p className="mt-4">
-              <strong>Result:</strong>{" "}
-              {(() => {
-                const netCalories =
-                  dietData.totalCaloriesConsumed -
-                  fitnessData.totalCaloriesBurned;
-                if (netCalories > caloriesRequired) {
-                  return (
-                    <span className="text-Quaternary font-bold">
-                      You are in a calorie surplus.{" "}
-                      {userInfo.journey === "WEIGHT_LOSS"
-                        ? "This may hinder weight loss."
-                        : userInfo.journey === "WEIGHT_GAIN"
-                        ? "Great for gaining weight!"
-                        : "You may gain weight."}
-                    </span>
-                  );
-                } else if (netCalories < caloriesRequired) {
-                  return (
-                    <span className="text-Quaternary font-bold">
-                      You are in a calorie deficit.{" "}
-                      {userInfo.journey === "WEIGHT_LOSS"
-                        ? "This is ideal for weight loss!"
-                        : userInfo.journey === "WEIGHT_GAIN"
-                        ? "This may hinder weight gain."
-                        : "You may lose weight."}
-                    </span>
-                  );
-                } else {
-                  return (
-                    <span className="text-Quaternary font-bold">
-                      You are meeting your maintenance calories.{" "}
-                      {userInfo.journey === "WEIGHT_LOSS"
-                        ? "Consider reducing your calories for weight loss."
-                        : userInfo.journey === "WEIGHT_GAIN"
-                        ? "Consider increasing your calories for weight gain."
-                        : "You are maintaining your weight."}
-                    </span>
-                  );
-                }
-              })()}
-            </p>
-          </div>
-        ) : (
-          <p className="text-Secondary">Calorie data is not available.</p>
-        )}
-      </div>
-      <h1 className="text-3xl underline font-semibold mt-6 text-center text-Quaternary mb-8">
-        Dashboard - Weekly Reports
-      </h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {/* Fitness Chart */}
-        <div className="bg-White bg-Grey bg-opacity-40 backdrop-blur-lg  p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold text-Quaternary mb-4">
-            Weekly Fitness Report
-          </h2>
-          <Bar data={fitnessChartData} options={{ responsive: true }} />
-        </div>
-
-        {/* Diet Chart */}
-        <div className="bg-White bg-Grey bg-opacity-40 backdrop-blur-lg  p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold text-Quaternary mb-4">
-            Weekly Diet Report
-          </h2>
-          <Line data={dietChartData} options={{ responsive: true }} />
-        </div>
-
-        {/* Sleep Chart */}
-        <div className="bg-White bg-Grey bg-opacity-40 backdrop-blur-lg  p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold text-Quaternary mb-4">
-            Weekly Sleep Report
-          </h2>
-          <Bar data={wellbeingChartData} options={{ responsive: true }} />
-        </div>
-      </div>
-      {/* Report Summary Section */}
-      <div className="bg-White bg-Grey bg-opacity-40 backdrop-blur-lg border-2 border-Quaternary p-6 rounded-lg shadow-md mb-8">
-        <h3 className="text-4xl font-semibold text-Quaternary mb-4">
-          Weekly Report Summary
-        </h3>
-
-        {/* Checking if reportSummary has data */}
-        {reportSummary.length > 0 ? (
-          <ul className="text-Secondary space-y-4">
-            {reportSummary.map((summary, index) => (
-              <li
-                key={index}
-                className="p-4 border-2 border-Quaternary rounded-lg"
-              >
-                <p>
-                  <strong>Calories Consumed This Week:</strong>{" "}
-                  <span className="text-Quaternary">
-                    {summary.CaloriesConsumedWeek}
-                  </span>
-                </p>
-                <p>
-                  <strong>Calories Burned This Week:</strong>{" "}
-                  <span className="text-Quaternary">
-                    {summary.CaloriesBurnedWeek}
-                  </span>
-                </p>
-                <p>
-                  <strong>Overall Status:</strong>{" "}
-                  <span
-                    className={`font-bold ${
-                      summary.Status === "Healthy"
-                        ? "text-green-500"
-                        : "text-red-500"
-                    }`}
-                  >
-                    {summary.Status}
-                  </span>
-                </p>
-                <p>
-                  <strong>Your Calories:</strong>{" "}
-                  <span className="text-Quaternary">
-                    {summary.UserCalories}
-                  </span>{" "}
-                  kcal
-                </p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-Secondary">No report summary available.</p>
-        )}
       </div>
     </div>
   );
